2b854475fe9bd54e9414c65f342685b1591fad43
José Yan <yanspfc97@gmail.com>
2024-08-09 08:56:55 -0300

    feat: enum to reuse http status codes

---
+++ src/lib/express/codes.ts
@@ -0,0 +1,13 @@
+export enum StatusCode {
+  ok = 200,
+  created = 201,
+  noBody = 204,
+  bad = 400,
+  forbidden = 403,
+  notFound = 404,
+  unprocessable = 422,
+  unauthorized = 401,
+  unknown = 500,
+}
+
+export const isSuccess = (code: number) => code >= 200 && code < 300;
