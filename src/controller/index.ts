9001b43d7585d883ad64d4caf6f5399f931127f8
José Yan <yanspfc97@gmail.com>
2024-08-09 08:52:55 -0300

    feat: create base controller for all controller classes

---
+++ src/controller/index.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import { StatusCode } from "../lib/express/codes";
+
+
+export type Action<T = Request> = (
+  req: T,
+  res: Response,
+  next: NextFunction
+) => any;
+
+export class BaseController {
+  sendCreated(res: Response, data?: any) {
+    return res.status(StatusCode.created).json(data);
+  }
+
+  sendSuccess(res: Response, data?: any) {
+    return res.status(StatusCode.ok).json(data);
+  }
+
+  sendVoid(res: Response) {
+    return res.status(StatusCode.noBody).send();
+  }
+}
