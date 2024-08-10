af01ab0a72522626ee04231cc6c813bae40b0c7b
José Yan <yanspfc97@gmail.com>
2024-08-10 16:45:08 -0300

    feat: initial implementation of generic request builder for shopify integration

---
+++ src/infrastructure/adapters/shopifyRequest.ts
@@ -0,0 +1,42 @@
+import { Headers } from "./types";
+
+export class ShopifyRequestBuilder {
+  public baseUrl: string;
+  public apiVersion: string;
+  public headers: Headers;
+  public body: Record<string, any>;
+
+  constructor() {
+    this.baseUrl = '';
+    this.headers = {
+      'Content-Type': 'application/json',
+      'X-Shopify-Access-Token': ''
+    };
+    this.apiVersion = ''
+    this.body = {};
+  }
+
+  public setBaseUrl(baseUrl: string): ShopifyRequestBuilder {
+    this.baseUrl = baseUrl;
+    return this;
+  }
+
+  public setHeaders(accessToken: string): ShopifyRequestBuilder {
+    this.headers['X-Shopify-Access-Token'] = accessToken;
+    return this;
+  }
+
+  public setBody(body: Record<string, any>): ShopifyRequestBuilder {
+    this.body = body;
+    return this;
+  }
+
+  public addBodyField(key: string, value: any): ShopifyRequestBuilder {
+    this.body[key] = value;
+    return this;
+  }
+
+  public build(): ShopifyRequestBuilder {
+    return this
+  }
+}
