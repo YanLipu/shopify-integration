ff54cb8dc250d7436857ebf9372a2c6e32670a6a
José Yan <yanspfc97@gmail.com>
2024-08-13 20:22:53 -0300

    chore: update eslint and prettier to latest version and update its configs

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ['node_modules', 'dist', 'public', 'coverage']
+  },
+  eslintConfigPrettier,
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      'sort-imports': [
+        'error',
+        {
+          ignoreCase: false,
+          ignoreDeclarationSort: false,
+          ignoreMemberSort: false,
+          memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+          allowSeparatedGroups: false
+        }
+      ]
+    }
+  }
+];
